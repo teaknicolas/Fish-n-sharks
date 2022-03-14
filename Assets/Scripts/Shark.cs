7351247014d4e89810b45cd9733ba901670d0858
Teak <teakguillaume@gmail.com>
2022-03-14 17:20:49 +0100

    Shark script
    Shark movements

--- Assets/Scripts/Shark.cs
+++ Assets/Scripts/Shark.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Shark : MonoBehaviour
 {
-    
+
+    public GameObject _player;
+    public Rigidbody2D _rigidbody2D;
+    private void Awake()
+    {
+        //_player = _player.GetComponent<GameObject>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        Vector2 dir = _player.transform.position - this.transform.position;
+        if(_player.tag == "Player")
+        {
+            _rigidbody2D.AddForce(dir);
+        }
+       
     }
 
     // Update is called once per frame
