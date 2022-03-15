8a988f5c08ae70131de4f9713086945c1f40d236
Teak <teakguillaume@gmail.com>
2022-03-15 17:00:41 +0100

    Basic ennemy done
    TO DO :
    
    - GameControllerScript to do

--- Assets/Scripts/Shark.cs
+++ Assets/Scripts/Shark.cs
@@ -7,9 +7,13 @@ public class Shark : MonoBehaviour
 
     public GameObject _player;
     public Rigidbody2D _rigidbody2D;
+
+    [SerializeField]
+    private float powerOfShark = 1;
     private void Awake()
     {
         //_player = _player.GetComponent<GameObject>();
+        _rigidbody2D = this.GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
@@ -19,7 +23,7 @@ public class Shark : MonoBehaviour
         Vector2 dir = _player.transform.position - this.transform.position;
         if(_player.tag == "Player")
         {
-            _rigidbody2D.AddForce(dir);
+            _rigidbody2D.AddForce(dir * powerOfShark) ;
         }
        
     }
