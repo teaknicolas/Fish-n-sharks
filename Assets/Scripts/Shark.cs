f91d7951abf1140d672e0484b0dd482d1f47775f
Teak <teakguillaume@gmail.com>
2022-03-29 15:18:53 +0200

    TO DO : - End game on ennemy collision
    Done : - Made a good homing missile behaviour for the ennemies

--- Assets/Scripts/Shark.cs
+++ Assets/Scripts/Shark.cs
@@ -3,30 +3,37 @@ using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class Shark : MonoBehaviour
 {
 
-    public GameObject _player;
-    public Rigidbody2D _rigidbody2D;
+    public Transform _player;
+    private Rigidbody2D rb;
+
+
 
     [SerializeField]
-    private float powerOfShark = 1;
+    private float speed = 1f;
+
+    [SerializeField]
+    private float rotateSpeed = 200f;
     private void Awake()
     {
         //_player = _player.GetComponent<GameObject>();
-        _rigidbody2D = this.GetComponent<Rigidbody2D>();
-        _player = GameObject.Find("Player");
+        rb = this.GetComponent<Rigidbody2D>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Start is called before the first frame update
     void Start()
     {
 
-        Vector2 dir = _player.transform.position - this.transform.position;
-        if (_player.tag == "Player")
-        {
-            _rigidbody2D.AddForce(dir * powerOfShark);
-        }
+        //Vector2 dir = _player.position - this.transform.position;
+        //if (_player.tag == "Player")
+        //{
+        //    _rigidbody2D.AddForce(dir * powerOfShark);
+        //}
 
     }
 
@@ -35,5 +42,17 @@ public class Shark : MonoBehaviour
     {
         //Vector2 dir = _player.transform.position - this.transform.position;
         //this.transform.position = this.transform.position * dir * Time.deltaTime * powerOfShark;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = (Vector2)_player.position - rb.position;
+
+        direction.Normalize();
+
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+        rb.angularVelocity = rotateSpeed *  - rotateAmount;
+        rb.velocity = transform.up * speed; 
     }
 }
